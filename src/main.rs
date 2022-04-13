efb1a6946a9449bb533a66a8a79921520fe647ae
Saverio Miroddi <saverio.pub2@gmail.com>
2022-04-13 14:50:13 +0200

    Add prototype Emojy searcher (hits bug!)
    This hits a [libxcf bug](https://bugs.launchpad.net/ubuntu/+source/git/+bug/1852985), that can't be properly worked around, as B/W emojis are pretty much unrecognizable (besides, removing the Google package has side effects).

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,19 @@ mod gui {
 }
 
 mod search {
+    pub mod emoji_searcher;
     pub mod searcher;
 }
 
 use gui::app_builder::AppBuilder;
 use gui::user_event_handler::UserEventHandler;
+use search::emoji_searcher::EmojiSearcher;
 use search::searcher::Searcher;
 
 fn main() {
     let (app, mut browser, receiver) = AppBuilder::build();
     let user_event_handler = UserEventHandler::new();
-    let searchers: Vec<Box<dyn Searcher>> = vec![];
+    let searchers: Vec<Box<dyn Searcher>> = vec![Box::new(EmojiSearcher::new())];
 
     while app.wait() {
         if let Some(event) = receiver.recv() {
