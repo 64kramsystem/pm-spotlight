dbda85604dc30ab32a9a448ffa9d2adc9aaf9904
Saverio Miroddi <saverio.pub2@gmail.com>
2022-04-13 18:17:09 +0200

    Add explanation of the current design to Searcher

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -1,5 +1,8 @@
 pub trait Searcher {
     fn handles(&self, pattern: &str) -> bool;
+    // Currently, a flat list of entries is returned, instead of a map, which would allow a view pattern.
+    // The result is that certain functionalities can't be implemented, e.g. a file search that doesn't
+    // display the whole path. This will be fixed once the list widget will implement such pattern.
     fn search(&self, pattern: &str) -> Vec<String>;
     fn execute(&self, entry: String);
 }
