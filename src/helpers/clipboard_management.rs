11b453604aceda21050397f4843a0b37da54c2e2
Saverio Miroddi <saverio.pub2@gmail.com>
2024-10-28 19:05:59 +0100

    Use "clipboard" crate (simpler and multiplatform)

--- src/helpers/clipboard_management.rs
+++ src/helpers/clipboard_management.rs
@@ -1,20 +1,12 @@
-use std::io::Write;
-use std::process::{Command, Stdio};
+use clipboard::{ClipboardContext, ClipboardProvider};
 
+// Copypasta was unstable; sometimes it didn't copy to clipboard, sometimes it has strange side
+// effects, like not pasting on the first paste invocation, or the paste being displayed in the
+// destination program only after other chars were typed.
+//
 pub fn copy_to_clipboard(text: String) {
-    // Copypasta was unstable; sometimes it didn't copy to clipboard, sometimes it has strange side
-    // effects, like not pasting on the first paste invocation, or the paste being displayed in the
-    // destination program only after other chars were typed.
-    //
-    let mut child = Command::new("xsel")
-        .arg("-ib")
-        .stdin(Stdio::piped())
-        .spawn()
-        .unwrap();
-    let mut child_stdin = child.stdin.take().unwrap();
-
-    write!(child_stdin, "{}", text).unwrap();
-
-    drop(child_stdin);
-    child.wait().unwrap();
+    let mut ctx: ClipboardContext =
+        ClipboardProvider::new().expect("Failed to initialize clipboard");
+    ctx.set_contents(text)
+        .expect("Failed to set clipboard contents");
 }
