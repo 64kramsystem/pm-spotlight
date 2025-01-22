bd953a25312487a7e7876d65a2d444436bea8730
Saverio Miroddi <saverio.pub2@gmail.com>
2025-01-22 16:31:46 +0100

    Clipboard manager: Workaround Clipboard issue, by restoring xsel strategy on Linux

--- src/helpers/clipboard_management.rs
+++ src/helpers/clipboard_management.rs
@@ -1,12 +1,39 @@
+// The main clipboard managers had issues:
+//
+// - Copypasta 0.71 was unstable; sometimes it didn't copy to clipboard, sometimes it has strange side
+//   effects, like not pasting on the first paste invocation, or the paste being displayed in the
+//   destination program only after other chars were typed.
+// - Clipboard 0.5.0 did not copy emojis on Linux (!)
+
+#[cfg(not(target_os = "linux"))]
 use clipboard::{ClipboardContext, ClipboardProvider};
 
-// Copypasta was unstable; sometimes it didn't copy to clipboard, sometimes it has strange side
-// effects, like not pasting on the first paste invocation, or the paste being displayed in the
-// destination program only after other chars were typed.
-//
+#[cfg(target_os = "linux")]
+use std::{
+    io::Write,
+    process::{Command, Stdio},
+};
+
 pub fn copy_to_clipboard(text: String) {
-    let mut ctx: ClipboardContext =
-        ClipboardProvider::new().expect("Failed to initialize clipboard");
-    ctx.set_contents(text)
-        .expect("Failed to set clipboard contents");
+    #[cfg(not(target_os = "linux"))]
+    {
+        let mut ctx: ClipboardContext =
+            ClipboardProvider::new().expect("Failed to initialize clipboard");
+        ctx.set_contents(text)
+            .expect("Failed to set clipboard contents");
+    }
+    #[cfg(target_os = "linux")]
+    {
+        let mut child = Command::new("xsel")
+            .arg("-ib")
+            .stdin(Stdio::piped())
+            .spawn()
+            .unwrap();
+        let mut child_stdin = child.stdin.take().unwrap();
+
+        write!(child_stdin, "{}", text).unwrap();
+
+        drop(child_stdin);
+        child.wait().unwrap();
+    }
 }
